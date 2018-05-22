34403038acddb9ca7dde62f482f85150323a66b4
vizuleanu <paliciucdan99@gmail.com>
2018-05-22 19:02:46 +0300

    Complete enemy constructor function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, moveSpeed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.moveSpeed = moveSpeed;
 };
 
 // Update the enemy's position, required method for game
